6c0aa796ac2fc70857bf54030f66d989deb9bf38
osyniaeva <dn100488rol@gmail.com>
2017-09-28 19:14:21 +0300

    demo for skills up students

---
+++ src/com/skillsup/syniaeva/SynchronizedBlockConcurrencyProblem.java
@@ -0,0 +1,49 @@
+package com.skillsup.syniaeva;
+
+/**
+ * Created by osyniaeva on 9/28/17.
+ */
+public class SynchronizedBlockConcurrencyProblem {
+	static int[] array = {0};
+
+	public static void main(String[] args) throws InterruptedException {
+		Thread a = new Thread() {
+			public void run() {
+				for (int i = 1; i <= 1000; i++) {
+					increase();
+				}
+			}
+		};
+
+		Thread b = new Thread() {
+			public void run() {
+				for (int i = 1; i <= 1000; i++) {
+					decrease();
+				}
+			}
+		};
+
+		a.start();
+		b.start();
+		a.join();
+		b.join();
+		System.out.println(array[0]);
+	}
+
+	public static void increase() {
+		// Do something else
+		synchronized (array) {
+			array[0]++;
+		}
+		// Do something else
+	}
+
+	public static void decrease() {
+		// Do something else
+		synchronized (array) {
+			array[0]--;
+		}
+		// Do something else
+	}
+}
+
---
+++ src/com/skillsup/syniaeva/SynchronizedConcurrencyProblem.java
@@ -0,0 +1,41 @@
+package com.skillsup.syniaeva;
+
+/**
+ * Created by osyniaeva on 9/28/17.
+ */
+public class SynchronizedConcurrencyProblem {
+	static int[] array = {0};
+
+	public static void main(String[] args) throws InterruptedException {
+		Thread a = new Thread() {
+			public void run() {
+				for (int i = 1; i <= 1000; i++) {
+					increase();
+				}
+			}
+		};
+
+		Thread b = new Thread() {
+			public void run() {
+				for (int i = 1; i <= 1000; i++) {
+					decrease();
+				}
+			}
+		};
+
+		a.start();
+		b.start();
+		a.join();
+		b.join();
+		System.out.println(array[0]);
+	}
+
+	public synchronized static void increase() {
+		array[0]++;
+	}
+
+	public synchronized static void decrease() {
+		array[0]--;
+	}
+}
+
